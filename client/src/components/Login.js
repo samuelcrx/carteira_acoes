ea0cf78f0b0b97fb2d8fa3ad58231d449e142fc6
Samuel Silva <samuelformal@gmail.com>
2020-09-14 20:10:50 -0300

    feat(login): Continua desenvolvimento da tela de Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,7 +4,9 @@ import {Button, FormGroup, FormControl, FormLabel } from 'react-bootstrap'
 
 import './Login.css'
 
-const Login = () => {
+const Logo = require('../assets/logo.jpg')
+
+const Login = props => {
     const [login, setLogin] = useState('')
     const [password, setPassword] = useState('')
     const [singinTouched, setSinginTouched] = useState(false)
@@ -20,15 +22,17 @@ const Login = () => {
     return(
         <div className='Container'>
             <div className='SubContainer'>
-                <div className='Logo'/>
-                <form onSubmit={() => onSubmit()}>
+                <div className='Logo'>
+                    <img src={Logo} alt="Logo" className='imagemLogo' />
+                </div>
+                <form className='formulario' onSubmit={() => onSubmit()}>
                     <FormGroup className='FormLogin' controlId='login' bsSize='large'>
                         <FormLabel className='TextLogin'>Login</FormLabel >
-                        <FormControl className='InputLogin' autoFocus type='email' value={login} onChange={event => setLogin(event.target.value)}/>
+                        <FormControl className='InputLogin' autoFocus type='email' placeholder='Digite seu E-mail' value={login} onChange={event => setLogin(event.target.value)}/>
                     </FormGroup>
                     <FormGroup controlId='password' bsSize='large'>
                         <FormLabel className='TextPass' >Senha</FormLabel >
-                        <FormControl className='InputPass' autoFocus type='password' value={password} onChange={event => setPassword(event.target.value)}/>
+                        <FormControl className='InputPass' autoFocus type='password' placeholder='Digite sua Senha' value={password} onChange={event => setPassword(event.target.value)}/>
                     </FormGroup>
                     <Button className={validateForm() ? 'ButtonLogin' : 'ButtonLoginDisabled'} block bsSize='large' disabled={!validateForm()} type='submit'>Entrar</Button>
                     <p className={singinTouched? 'TextSingin TextSinginTouched' : 'TextSingin'} onMouseUp={() => setSinginTouched(false)} onMouseDown={() => setSinginTouched(true)} onClick={() => alert('going to singin screen')}>Criar Conta</p>
