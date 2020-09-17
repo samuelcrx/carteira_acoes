f72af3753c704978cec4ce794b4bfa3b23c86ed9
Rocetti Rinke <ro7rinke@gmail.com>
2020-09-17 17:32:45 -0300

    tela cadastro e router adicionados

--- client/src/components/Cadastro.js
+++ client/src/components/Cadastro.js
@@ -1,15 +1,19 @@
 import React, {useState} from 'react'
 
 import {Button, FormGroup, FormControl, FormLabel } from 'react-bootstrap'
+
 import {Link} from 'react-router-dom'
+
+import './Cadastro.css'
 
 import './Login.css'
 
 const Logo = require('../assets/logo.jpg')
 
-const Login = props => {
+const Cadastro = props => {
     const [login, setLogin] = useState('')
     const [password, setPassword] = useState('')
+    const [nome, setNome] = useState('')
     const [singinTouched, setSinginTouched] = useState(false)
 
     const validateForm = () => {
@@ -27,6 +31,10 @@ const Login = props => {
                     <img src={Logo} alt="Logo" className='imagemLogo' />
                 </div>
                 <form className='formulario' onSubmit={() => onSubmit()}>
+                    <FormGroup className='FormNome' controlId='nome' bsSize='large'>
+                        <FormLabel className='TextLogin'>Nome</FormLabel >
+                        <FormControl className='InputLogin' autoFocus type='email' placeholder='Digite seu Nome' value={nome} onChange={event => setNome(event.target.value)}/>
+                    </FormGroup>
                     <FormGroup className='FormLogin' controlId='login' bsSize='large'>
                         <FormLabel className='TextLogin'>Login</FormLabel >
                         <FormControl className='InputLogin' autoFocus type='email' placeholder='Digite seu E-mail' value={login} onChange={event => setLogin(event.target.value)}/>
@@ -35,21 +43,15 @@ const Login = props => {
                         <FormLabel className='TextPass' >Senha</FormLabel >
                         <FormControl className='InputPass' autoFocus type='password' placeholder='Digite sua Senha' value={password} onChange={event => setPassword(event.target.value)}/>
                     </FormGroup>
-                    <Button className={validateForm() ? 'ButtonLogin' : 'ButtonLoginDisabled'} block bsSize='large' disabled={!validateForm()} type='submit'>Entrar</Button>
-                    <Link to='/cadastro'>
-                        <p className={singinTouched? 'TextSingin TextSinginTouched' : 'TextSingin'} onMouseUp={() => setSinginTouched(false)} onMouseDown={() => setSinginTouched(true)} >Criar Conta</p>
+                    <Button className={validateForm() ? 'ButtonLogin' : 'ButtonLoginDisabled'} block bsSize='large' disabled={!validateForm()} type='submit'>Cadastrar</Button>
+                    <Link to='/'>
+                    <p className={singinTouched? 'TextSingin TextSinginTouched' : 'TextSingin'} onMouseUp={() => setSinginTouched(false)} onMouseDown={() => setSinginTouched(true)} >Login</p>
                     </Link>
                 </form>
-                {/* <p className='TextLogin'>Login</p>
-                <input className='InputLogin'/>
-                <p className='TextPass'>Senha</p>
-                <input className='InputPass'/>
-                <button className='ButtonLogin'>Entrar</button>
-                <p onClick={() => alert('ueeeeeee')} className='TextSingin'>Criar Conta</p> */}
             </div>
         </div>
     )
 
 }
 
-export default Login
+export default Cadastro
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import Login from './components/Login';
+import Cadastro from './components/Cadastro'
+
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact component={Login}/>
+        <Route path='/cadastro' component={Cadastro} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
