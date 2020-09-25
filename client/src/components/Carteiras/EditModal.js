247b8611e62e0295fd5f56868e7d7f277a413f8e
Rocetti Rinke <ro7rinke@gmail.com>
2020-09-24 21:32:39 -0300

    botoes adicionados ao modal, css do campo de descricao incompleto

--- client/src/components/Carteiras/EditModal.js
+++ client/src/components/Carteiras/EditModal.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -16,11 +17,84 @@ const useStyles = makeStyles((theme) => ({
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
+  container:{
+    width: "730px",
+    height: "320px",
+    left: "0px",
+    top: "0px",
+    background: "#343A40",
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  subContainer: {
+    width: '700px',
+    height: '277px',
+    left: '16px',
+    top: '20px',
+    background: '#37404A',
+    boxShadow: '0px 2px 1px rgba(0,0,0,0.25)'
+  },
+  titleContainer: {
+    width: '100%',
+    height: '36px',
+    background: '#3c4858',
+    boxShadow: '0px 4px 4px rgba(0,0,0,0.25)',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  titleText: {
+    fontFamily: 'Nunito',
+    fontStyle: 'normal',
+    fontSize: '14px',
+    lineHeight: '19px',
+    paddingLeft: '12px',
+    // display: 'flex',
+    // alignItems: 'center',
+    color: 'white'//'linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%), #AAB8C5'
+  },
+  btBack:{
+    background: '#0F7979',
+    borderRadius: '8px',
+    color: 'white',
+    top: '20px',
+    left: '20px'
+  }, 
+  btSave:{
+    background: '#727CF5',
+    borderRadius: '8px',
+    color: 'white',
+    top: '20px',
+    left: '30%',
+  },
+  btExclude:{
+    background: '#FA5C7C',
+    borderRadius: '8px',
+    color: 'white',
+    top: '20px',
+    left: '33%',
+  },
+  btContainer: {
+    top: '20px'
+  },
+  dataContainer: {
+    background: '#394451',
+    border: '1px',
+    boxSizing: 'border-box',
+    boxShadow: 'inset 0px 0px 2px rgba(0, 0, 0, 0.25)',
+    width: '90%',
+    height: '40%'
+  },
+  descriptionText: {
+    fontFamily: 'Nunito',
+    fontSize: '14px',
+    fontStyle: 'normal'
+  }
 }));
 
 export default function EditModal() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(true);
 
   const handleOpen = () => {
     setOpen(true);
@@ -48,10 +122,23 @@ export default function EditModal() {
         }}
       >
         <Fade in={open}>
-          <div className={classes.paper}>
-            <h2 id="transition-modal-title">Transition modal</h2>
-            <p id="transition-modal-description">react-transition-group animates me.</p>
-          </div>
+          {/* <div className={classes.paper}> */}
+            <div className={classes.container}>
+              <div className={classes.subContainer}>
+                <div className={classes.titleContainer}>
+                  <p className={classes.titleText}>Carteira de Ações</p>
+                </div>
+                <div className={classes.btContainer}>
+                  <Button className={classes.btBack}>Voltar</Button>
+                  <Button className={classes.btSave}>Salvar</Button>
+                  <Button className={classes.btExclude}>Excluir</Button>
+                </div>
+                <div className={classes.dataContainer}>
+
+                </div>
+              </div>
+            </div>
+          {/* </div> */}
         </Fade>
       </Modal>
     </div>
